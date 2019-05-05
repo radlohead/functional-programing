c41f2a65287bac9c7b7bfbe8b3f96759dfa72fe6
minho <radlohead@naver.com>
2019-05-05 16:23:05 +0900

    package.json needless module delete

--- compile/curry-compiled.js
+++ compile/curry-compiled.js
@@ -1,3 +1,9 @@
 "use strict";
 
 var _libCompiled = require("./lib-compiled.js");
+
+var add = function add(a, b) {
+  return a + b;
+};
+
+console.log((0, _libCompiled.curry)(add)([2, 5]));
--- es6/curry.js
+++ es6/curry.js
@@ -1,1 +1,4 @@
 import { curry } from './lib-compiled.js'
+
+const add = (a, b) => a + b
+console.log(curry(add)([2, 5]))
